bbeb5f24f35bc60080c56c7674064ff9dd31075b
addriadrie <143043838+addriadrie@users.noreply.github.com>
2024-03-03 08:46:01 +0800

    Add files via upload

--- patient-booking.php
+++ patient-booking.php
@@ -4,57 +4,23 @@
     $pntID = $_GET['patientID']; // fetched ID
     $svcID = $_GET['serviceID']; // fetched ID
 
-    // get patient info to print in form
+    // get patient info
     $sql = "SELECT patientID, firstName, middleName, lastName, email FROM tblusers WHERE patientID='$pntID';"; 
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $patientID = $row["patientID"];
+    $pntID = $row["patientID"];
     $fname = $row["firstName"];
     $midname = $row["middleName"];
     $lname = $row["lastName"];
     $email = $row["email"];
 
-    // get service info to print in form
+    // get service info
     $sql = "SELECT tblservice.serviceID, tblservice.serviceName, tblcategory.categName FROM tblservice INNER JOIN tblcategory WHERE serviceID='$svcID';"; 
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $serviceID = $row["serviceID"];
+    $svcID = $row["serviceID"];
     $svcName = $row["serviceName"];
     $ctgName = $row["categName"];
-
-    
-    if(isset($_POST['submit'])){
-        // fetching date and note upon submission
-        $date = $_POST['date'];
-        $note = $_POST['note'];
-
-        // generating appt id
-        $prefix = date("Ymd", strtotime($date));
-        $sql = "SELECT MAX(apptID) AS latest FROM tblappt WHERE apptDate = '$date'";
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-
-        if ($row['latest'] === null) {
-            // No appt made on the current date
-            $count = 1;
-            $length = 3;
-            $apptID = $prefix . str_pad($count, $length, '0', STR_PAD_LEFT);
-        } else {
-            $latest = intval($row['latest']);
-            $apptID = $latest +1;
-        }
-
-        // inserting to appointment table
-        $sql = "INSERT INTO tblappt(apptID, patientID, serviceID, apptDate, patientNote, statusID) VALUES ('$apptID', $patientID,'$serviceID', '$date', '$note', 1)";
-        $result = $conn->query($sql);
-
-        // if successful insertion
-        if ($result) {
-            header('location: patient-appt.php?patientID='.$patientID);
-        } else {
-            die("Connection Failed: " . $conn->connect_error);        
-        }
-    }
 ?>
 
 <!DOCTYPE html>
@@ -112,78 +78,74 @@
             font-size: 14px;
         }
 
-        .form-label {
-            font-family: "DM Sans"; 
-            font-size: 14px;
-            margin-top: 5px;
-        }
-
         .form-control {
             font-family: "DM Sans"; 
             font-size: 16px;
         }
 
+        .info {
+            width: 100%;
+        }
+
+        .button {
+            border-radius: 30px;
+            padding: 10px;
+            color: white;
+            background-color: #BE9355;
+            border-color: transparent;
+        }
+        .footer {
+            background-color: #f4f5f6;
+            font-family: DM Sans;
+        }
+        .footer-title {
+            font-family: Poppins;
+            font-weight: bold;
+            color: #BE9355;
+            font-size: 18px;
+        }
+        .icons {
+            float: right;
+        }
+
+        .reco {
+            font-family: DM Sans;
+            font-weight: bold;
+        }
+
+        .col {
+            font-family: DM Sans;
+            font-size: 15px;
+        }
+        input[type="date"] {
+            font-size: 16px;
+            width: 325px;
+        }
         .sched-title {
             font-family: DM Sans; 
             font-size: 18px; 
             font-weight: bold; 
             color: #BE9355;
-            display: inline-block;
-            margin-left: 20px;
-            margin-bottom: 5px;
-        }
-
-        .sched-time {
-            font-family: "Poppins";
-            font-size: 15px;
-            background-color: white;
+            background-color: #f5f5f5;
             width: 310px;
-            padding-top: 5px;
-            padding-left: 10px;
-            padding-bottom: 5px;
-            margin-left: 20px;
-            margin-bottom: 5px;
+            margin-left: 10px;
+            text-align: left;
         }
-
-        .sched-note {
-            font-family: "Poppins";
-            font-size: 13px;
-            font-style: italic;
+        .sched-time {
+            background-color: white;
+            width: 280px;
+            text-align: left;
             margin-left: 25px;
+            margin-bottom: 0%;
         }
-
-        .alert {
-            font-family: "DM Sans";
-            margin-top: 25px;
-            padding-top: 10px;
-            padding-bottom: 10px;
+        .sched-note {
+            text-align: left;
+            margin-left: 25px;
+            font-size: 12px;
+            width: 280px;
         }
-
-        .button {
-            font-family: "DM Sans";
-            color: white;
-            background-color: #BE9355;
-            border-radius: 12px;
-            padding: 8px;  
-            border-color: transparent;
-            float: right;
-            margin-right: 15px;
-        }
-
-        .footer {
-            background-color: #f4f5f6;
-            font-family: DM Sans;
-        }
-        
-        .footer-title {
-            font-family: Poppins;
-            font-weight: bold;
-            color: #BE9355;
-            font-size: 16px;
-        }
-
-        .footer-icons {
-            float: right;
+        ul {
+            list-style-type: none;
         }
 
     </style>
@@ -203,7 +165,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="patient-home.php?patientID=<?php echo $patientID ?>">Home</a>
+                    <a class="nav-link" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Appointments</a>
@@ -241,77 +203,89 @@
 
     <br>
 
-    <!-- FORM -->
     <div class="container">
-        <p style="font-family:DM Sans; text-align:center; color:#BE9355; font-weight:bold; font-size:24px">Book Appointment</p><br>
-
-        <div class="row">
-            <!-- Two-thirds width column -->
-            <div class="col-md-8">
-                <div class="service-row">
-                    <div class="form-group col-md-6">
+        <p style="font-family:DM Sans; text-align:center; color:#BE9355; font-weight:bold; font-size:24px">Book Appointment</p> <br>
+        <ul>
+            <li>
+                <div class="row align-items-start">
+                    <div class="col">
                         <input class="form-control" type="text" value="<?php echo $ctgName ?>" aria-label="Disabled input example" disabled readonly name="ctgName">
                     </div>
-                    <div class="form-group col-md-6">
+                    <div class="col">
                         <input class="form-control" type="text" value="<?php echo $svcName ?>" aria-label="Disabled input example" disabled readonly name="svcName"> 
                     </div>
-                </div>
-                <div class="name-row">
-                    <div class="form-group col-md-4">
-                        <label class="form-label">First Name</label>
-                        <input type="text" class="form-control" value="<?php echo $fname ?>" aria-label="Disabled input example" disabled readonly name="fname">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label class="form-label">Middle Name</label>
-                        <input type="text" class="form-control" value="<?php echo $midname ?>" aria-label="Disabled input example" disabled readonly name="midname">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label class="form-label">Last Name</label>
-                        <input type="text" class="form-control" value="<?php echo $lname ?>" aria-label="Disabled input example" disabled readonly name="lname">
+                    <div class="col">
+                        <input type="date" id="txtDate" min="<?php echo date("Y-m-d"); ?>" name="date"> </li>
                     </div>
                 </div>
-                <form method="post">
-                    <div class="email-row">
-                        <div class="form-group col-md-6">
-                            <label class="form-label">Email</label>
-                            <input type="text" class="form-control" value="<?php echo $email ?>" aria-label="Disabled input example" disabled readonly name="email">  
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label class="form-label">Select Date</label>
-                            <input type="date" class="form-control" min="<?php echo date("Y-m-d"); ?>" name="date" required>
-                        </div>
-                    </div>
-                    <div class="third-row">
-                        <div class="form-group col-md-12">
-                            <label class="form-label">Additional Note</label>
-                            <textarea class="form-control" placeholder="Please indicate any special instructions such as allergies, pregnancy, and the like." name="note" style="font-size: 13px; height: 120px;"></textarea>
-                            
-                        </div>
-                    </div>
-                    <button type="submit" class="button" name="submit">Confirm</button>
-                </form>
-            </div>
-            <!-- One-third width column -->
-            <div class="col-md-4">
-                <div class="schedule" style="background-color: #f4f5f6;">
-                    <br>
-                    <p class="sched-title">MON - SAT</p>
-                    <p class="sched-time"><b>10:00AM - 8:00PM</b><br>Walk-In and Appointment</p>
-                    <p class="sched-note">For consultation, you may drop by anytime between 2:00PM to 8:00PM</p>
-                    <p class="sched-title">SUN</p>
-                    <p class="sched-time"><b>10:00AM - 8:00PM</b><br>Walk-In and Appointment</p>
-                    <p class="sched-note">Appointment for consultation</p>
-                    <br>
-                </div>
-                <div class="alert alert-info">
-                    <i class="fa fa-exclamation-circle fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;Please be informed that confirmed appointments can not be cancelled. For queries, contact us through the provided information below.
-                </div>
-            </div>
-        </div>
-    </div>
-      
-    <br><br><br><br><br>
+            </li>
+            <li>
+                <div class="container">
+                    <div class="row align-items-start">
+                        <div class="col-sm-8 p-3">
+                            <ul>
+                                <li>
+                                    <div class="row align-items-start">
+                                        <div class="col">
+                                            <label class="form-label">First Name</label>
+                                            <input type="text" class="form-control" value="<?php echo $fname ?>" name="fname">
+                                        </div>
+                        
+                                        <div class="col">
+                                            <label class="form-label">Middle Name</label>
+                                            <input type="email" class="form-control" value="<?php echo $midname ?>" id="midname">
+                                        </div>
 
+                                        <div class="col">
+                                            <label class="form-label">Last Name</label>
+                                            <input type="email" class="form-control" value="<?php echo $lname ?>" id="lname">
+                                        </div>
+
+                                    </div>
+                                </li>
+                                <li>
+                                    <br>
+                                    <h4 style="font-family: DM Sans; font-weight:bold;">Additional Note</h4>
+                                    <div class="form-floating">
+                                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="note"></textarea>
+                                        <label style="font-weight: lighter; font-family: DM Sans; color:#D3D3D3;">Please indicate any special instructions such as allergies, pregnancy, and the like.</label>
+                                    </div>
+                                    <br>
+                                    <button type="submit" class="btn btn-warning" style="font-family: DM Sans;">Submit</button>
+                                    <br><br>
+                                </li>
+                            </ul>                                               
+                        </div>
+                        <div class="col-4 p-3 mb-2">
+                        <div class="col">
+                            <ul> 
+                                <div class="sched" style="background-color: #f5f5f5;">
+                                    <li> 
+                                        <br>
+                                        <p class="sched-title">&nbsp;&nbsp;&nbsp;MON - SAT</p>
+                                        <p class="sched-time">&nbsp;10:00AM - 8:00PM <br> &nbsp;Walk-In and Appointment</p>
+                                        <p class="sched-note">For consultation, you may drop by anytime between 2:00PM to 8:00PM</p>
+                                        &nbsp;
+                                    </li>
+                                </div>
+                                <div class="sched" style="background-color: #f5f5f5; margin-left: -110px; width: 325px;">
+                                    <li> 
+                                        <p class="sched-title">&nbsp;&nbsp;&nbsp;SUN</p>
+                                        <p class="sched-time">&nbsp;10:00AM - 8:00PM <br> &nbsp;Walk-In and Appointment</p>
+                                        <p class="sched-note">Appointment for consultation</p>
+                                        <br>
+                                    </li>
+                                </div>
+                            </ul>
+                            <br><br>   
+                        </div>
+                        </div>
+                    </div>
+                </div>            
+            </li>
+    </div>
+            
+    <br><br>
     <!-- FOOTER -->
     <footer>
         <div id="contact" class="footer">
@@ -320,19 +294,19 @@
                     <div class="col-12 col-sm-6 col-md-8">
                         <br> <br> 
                         <p class="footer-title">Contact Us</p>
-                        <p class="footer-address" style="font-size: 14px;">2/F 1 Cirq Building, Sen. Lorenzo Sumulong Avenue, Brgy. San Roque, Antipolo, Philippines</p>
+                        <p class="footer-address" style="font-size: 16px;">2/F 1 Cirq Building, Sen. Lorenzo Sumulong Avenue, Brgy. San Roque, Antipolo, Philippines</p>
                         <br><br>
                     </div>
                     <div class="col-6 col-md-4">
                         <p>
-                            <div class=" footer-icons">
+                            <div class="icons">
                                 <br><br>
                                 <a href="https://www.facebook.com/Derma101" style="color: #BE9355;"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
-                                <a href="mailto:derma101ph@yahoo.com" style="color: #BE9355;"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
-                                <a href="https://www.derma101ph.com" style="color: #BE9355;"><i class="fa fa-globe fa-2x" aria-hidden="true"></i></a>                  
+                                <a href="derma101ph@yahoo.com" style="color: #BE9355;"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                                <a href="derma101ph.com" style="color: #BE9355;"><i class="fa fa-link fa-2x" aria-hidden="true"></i></a>                  
                             </div>
                         </p> <br><br><br><br>
-                        <p class="footer-copyright" style="text-align: right; color: #C0C0C0; font-size: 12px;">Copyright © 2024. All rights reserved.</p>
+                        <p class="footer-copyright" style="text-align: right; color: #C0C0C0;">Copyright © 2024. All rights reserved.</p>
                     </div>
                 </div>           
             </div>     
