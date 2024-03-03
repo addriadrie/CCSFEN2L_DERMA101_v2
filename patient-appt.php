8e05e32af2355189442891ee809d3feff7795e36
addriadrie <143043838+addriadrie@users.noreply.github.com>
2024-03-03 09:55:40 +0800

    Add files via upload

--- patient-appt.php
+++ patient-appt.php
@@ -59,6 +59,60 @@
             font-family: DM Sans;
             font-size: 13px;
         }
+
+        .card {
+            border: 2px solid #BE9355;
+            border-radius: 10px;
+            width: 80%;
+            margin: 0 auto;
+        }
+
+        .card-header {
+            font-family: 'Poppins';
+            font-weight: bold;
+            font-size: 16px;
+            background-color: rgb(225, 172, 65, .5);
+            border-bottom: 2px solid #BE9355;
+            padding: 10px;
+            padding-left: 50px;            
+        }
+
+        .title {
+            font-family: 'DM Sans';
+            font-size: 16px;
+            font-weight: lighter;
+            color: #BE9355;
+            padding-left: 15%; 
+            margin-bottom: 0;
+        }
+
+        .sub {
+            font-family: 'DM Sans';
+            font-size: 16px;
+            font-weight: bold;
+            padding-left: 15%;
+            margin-right: 30px;
+        }
+    
+        .footer {
+            background-color: #f4f5f6;
+            font-family: DM Sans;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+        }
+        
+        .footer-title {
+            font-family: Poppins;
+            font-weight: bold;
+            color: #BE9355;
+            font-size: 16px;
+        }
+
+        .footer-icons {
+            float: right;
+        }
+
     </style>
 
     <title>Appointments</title>
@@ -124,12 +178,12 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-4">
-                                <p>Appointment ID</p>
-                                <p>'.$apptID.'</p>
+                                <p class="title">APPOINTMENT ID</p>
+                                <p class="sub">'.$apptID.'</p>
                             </div>
                             <div class="col-md-4">
-                                <p>Service</p>
-                                <p>'?><?php
+                                <p class="title">SERVICE</p>
+                                <p class="sub">'?><?php
                                 $svcsql = "SELECT serviceName FROM tblservice WHERE serviceID='$svcID';"; 
                                 $svcresult = $conn->query($svcsql);
                                 $svcrow = $svcresult->fetch_assoc();
@@ -138,8 +192,8 @@
                                 ?><?php echo'</p>
                             </div>
                             <div class="col-md-4">
-                                <p>Status</p>
-                                <p>'?><?php
+                                <p class="title">STATUS</p>
+                                <p class="sub">'?><?php
                                 $stsql = "SELECT tblstatus.status FROM tblstatus WHERE statusID='$statusID';"; 
                                 $stresult = $conn->query($stsql);
                                 $strow = $stresult->fetch_assoc();
@@ -149,13 +203,43 @@
                             </div>
                         </div>
                         <div>
-                            <p>Note</p>
-                            <p>'.$note.'</p>
-                        </div>                      
+                            <p class="title" style="padding-left: 38px">NOTE</p>
+                            <p class="sub" style="padding-left: 38px">'.$note.'</p>                                                 
+                        </div>                                    
                     </div>
                 </div> <br>';      
         } ?>
     </div>
+
+    <br><br><br>
+
+    <!-- FOOTER -->
+    <footer>
+        <div id="contact" class="footer" style="width: 100%;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-8">
+                        <br> <br> 
+                        <p class="footer-title">Contact Us</p>
+                        <p class="footer-address" style="font-size: 14px;">2/F 1 Cirq Building, Sen. Lorenzo Sumulong Avenue, Brgy. San Roque, Antipolo, Philippines</p>
+                        <br><br>
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <p>
+                            <div class=" footer-icons">
+                                <br><br>
+                                <a href="https://www.facebook.com/Derma101" style="color: #BE9355;"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                                <a href="mailto:derma101ph@yahoo.com" style="color: #BE9355;"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                                <a href="https://www.derma101ph.com" style="color: #BE9355;"><i class="fa fa-globe fa-2x" aria-hidden="true"></i></a>                  
+                            </div>
+                        </p> <br><br><br><br>
+                        <p class="footer-copyright" style="text-align: right; color: #C0C0C0; font-size: 12px;">Copyright © 2024. All rights reserved.</p>
+                    </div>
+                </div>           
+            </div>     
+        </div>           
+    </footer>
+
 
 </body>
 </html>
